145d7d296705f69e1afed655aad9ef5e8c8bcd5a
Adeola Bamigboye <Adeola.Bamigboye@prisma-capacity.eu>
2021-12-06 18:42:35 +0100

    prepare for deploy

--- server.js
+++ server.js
@@ -165,7 +165,15 @@ app.post("/sub", async (req, res) => {
   res.json({ client_secret: client_secret, status: status });
 });
 
+// serve static assests in prod
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+
+  app.use(express.static('client/build'))
+
+  app.get('*',(req, res) =>res.sendFile(path.resolve(__dir, 'client', 'build', 'index.html')) )
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
